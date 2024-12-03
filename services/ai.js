409712311edc8d093bd4ef9b6d0f695a73572b13
Adam Driggers <awdriggs@gmail.com>
2024-12-03 20:34:11 +0100

    commit before rebase

--- services/ai.js
+++ services/ai.js
@@ -16,18 +16,25 @@ const openai = new OpenAI({
 export const getImage = async (caption) => {
   //get a dall-e image
   console.log("getting image from caption");
-  const image = await openai.images.generate({
-    model: "dall-e-3",
-    prompt: caption,
-    n: 1,
-    size: "1024x1024",
-    response_format: "b64_json",
-    style: "vivid"
-  })
-   
-  // console.log(image);
-  console.log(image); 
-  return image.data[0].b64_json; 
+
+  //
+  if(caption.includes("sorry")){ //test this catch, if the caption says "i'm sorry" then dall-e throws an error
+    return "";
+  } else {
+
+    const image = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: caption,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+      style: "vivid"
+    })
+
+    // console.log(image);
+    console.log(image);
+    return image.data[0].b64_json;
+  }
 };
 
 export const getCaption = async (img) => {
@@ -39,7 +46,7 @@ export const getCaption = async (img) => {
       {
         role: "user",
         content: [
-          { type: "text", text: "Describe what you see in this photo. Be as specific as possible. If there is a person describe what they look like, including their preceived gender, age and race." },
+          { type: "text", text: "Describe what you see in this photo. Be as specific as possible. If there is a person describe what they look like, including their preceived gender, age and race. Begin your response with 'I see'" },
           {
             type: "image_url",
             image_url: {
@@ -53,7 +60,7 @@ export const getCaption = async (img) => {
     ],
   });
 
-  console.log(response.choices[0]);
+  // console.log(response.choices[0]);
   return response.choices[0].message.content;
 }
 
